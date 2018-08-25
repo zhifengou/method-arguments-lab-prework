9f5b021fb0c60466e14bbcad30a347506721a977
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-25 14:24:16 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name}.\n and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
